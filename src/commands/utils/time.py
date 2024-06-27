764aa128ac1a2bf7883e14722124fc11d3a78569
Phoenix Isaac Pereira <phoenixpereira99@gmail.com>
2024-06-27 17:39:16 +0930

    chore: Replace utcnow with now

--- src/commands/utils/time.py
+++ src/commands/utils/time.py
@@ -4,6 +4,6 @@ import pytz
 def get_adelaide_time():
     # Convert UTC timestamp to Adelaide time
     adelaide_tz = pytz.timezone('Australia/Adelaide')
-    utc_now = datetime.datetime.utcnow()
-    adelaide_time = utc_now.replace(tzinfo=pytz.utc).astimezone(adelaide_tz)
+    utc_now = datetime.datetime.now(pytz.utc)
+    adelaide_time = utc_now.astimezone(adelaide_tz)
     return adelaide_time
